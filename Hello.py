8df3c298031ec29790f7e4e1134c9db7a777db22
Debbie Matthews <debbie.matthews@snowflake.com>
2023-09-19 09:11:38 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋:balloon:")
+    st.write("# Welcome to Streamlit! 👋")
 
     st.sidebar.success("Select a demo above.")
 
